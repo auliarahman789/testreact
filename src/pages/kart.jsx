ccc5fc891a8ec5631dae556b12a087613c1d91a1
Muhammad Aulia Rahman, S.Kom. <mauliarahman214@gmail.com>
2024-03-20 10:00:23 +0700

    Initial

---
+++ src/pages/kart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Kart() {
+    return (
+        <div className='flex px-16 py-12'>
+            <main className='flex w-full h-[650px] rounded-xl shadow-xl bg-slate-300 pt-10 text-red-500 justify-center px-16 py-16'>
+
+            </main>
+        </div>
+    )
+}
+
+export default Kart
